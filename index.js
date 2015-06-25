f9cfa6154c6dcba82729057c8b763b9922dd4c23
Shane Curran <shane@velodro.me>
2015-06-25 15:29:32 +0100

    Implemented new API endpoints

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ var request = require("request");
 function Velodrome(public_key, private_key) {
   this.config = {
     api: {
-      api_host: "https://api.velodro.me",
+      api_protocol: "https://",
+      api_host: "api.velodro.me",
       api_base_url: "",
       keys: {
         public_key: public_key,
@@ -23,43 +24,83 @@ function Velodrome(public_key, private_key) {
       auth_token: "Basic " + new Buffer(public_key + ":" + private_key).toString("base64")
     }
   }
+
+  request.get({
+    "uri": this.buildUrl("/account"),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }, function (error, response, data) {
+    if (error) {
+      throw error;
+    } else if (response.code == 200 && data && data !== "Unauthorized") {
+      // All good
+    } else if (data && data === "Unauthorized") {
+      throw new Error("Invalid credentials");
+    }
+  });
 }
 
 Velodrome.prototype.buildUrl = function (route) {
-  return this.config.api.api_host + this.config.api.api_base_url + route;
+  return this.config.api.api_protocol + this.config.api.keys.public_key + ":" + this.config.api.keys.private_key + "@" + this.config.api.api_host + this.config.api.api_base_url + route;
 };
 
-Velodrome.prototype.sendRequest = function (method, route, fields = []) {
+Velodrome.prototype.sendRequest = function (method, route, callback, fields) {
   // Build a URL
   var url = this.buildUrl(route);
 
-  request({
+  var options = {
     "method": method,
     "uri": url,
     headers: {
-       "Content-Type": "application/json"
-     }
-  }, function (error, response, body) {
-    if (error) {
-      throw error;
-    } else if (response && response.statusCode == 200) {
-      return body;
-    } else if (response) {
-      return response;
-    } else {
-      return {};
+       "Content-Type": "application/json",
+       "Authorization": this.config.api.auth_token
     }
-  });
+  };
 
+  if (method === "POST") {
+    options.form = fields;
+  }
+
+  request(options, function (error, response, data) {
+    // Ternary operators are bad
+    callback(error, response, (JSON.parse(data) ? JSON.parse(data) : data));
+    //                                          ^                  ^
+  });
 }
 
 Velodrome.prototype.echo = function () {
   return this;
 }
 
-Velodrome.prototype.getAccount = function () {
+Velodrome.prototype.getAccount = function (callback) {
   // Fetch the user's account
+  this.sendRequest("GET", "/account", callback);
 }
 
+Velodrome.prototype.listCities = function (callback) {
+  // Fetch the list of cities
+  this.sendRequest("GET", "/cities", callback);
+}
+
+Velodrome.prototype.listCards = function (callback) {
+  // Fetch the list of cards associated with the user's account
+  this.sendRequest("GET", "/customer/cards", callback);
+}
+
+Velodrome.prototype.newQuote = function (options, callback) {
+  // Create a new quote based on the parameters set
+  this.sendRequest("POST", "/customer/quotes", callback, options);
+}
+
+Velodrome.prototype.newOrder = function (options, callback) {
+  // Create a new order based on the parameters set
+  this.sendRequest("POST", "/customer/orders", callback, options);
+}
+
+Velodrome.prototype.listQuotes = function (callback) {
+  // Fetch the list of a customer's quotes and return them
+  this.sendRequest("GET", "/customer/quotes", callback);
+}
 
 module.exports = Velodrome;
